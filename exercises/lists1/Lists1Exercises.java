399266f6c5ead1aa732caa0a4297bb163d98af64
yuf0108 <yft504927@gmail.com>
2026-08-22 16:22:46 -0400

    Implement incrList and dincrList methods
    Fix null checks and implement list increment functions.

--- exercises/lists1/Lists1Exercises.java
+++ exercises/lists1/Lists1Exercises.java
@@ -4,7 +4,13 @@ public class Lists1Exercises {
       * to change. */
     public static IntList incrList(IntList L, int x) {
         /* Your code here. */
-        return L;        
+        if(L = null){
+            return null;
+        }
+        
+        return new IntList(L.first + x, 
+        dincrList(L.rest, x);
+        
     }
 
     /** Returns an IntList identical to L, but with
@@ -12,6 +18,12 @@ public class Lists1Exercises {
       * the 'new' keyword. */
     public static IntList dincrList(IntList L, int x) {
         /* Your code here. */
+        if(L = null){
+            return null;
+        }
+        L.first + = x;
+        dincrList(L.rest, x);
+        
         return L;
     }
 
@@ -27,7 +39,7 @@ public class Lists1Exercises {
         // code for incrList and dincrList into IntList.java and
         // run it in the visualizer.
         // System.out.println(L.get(1));
-        // System.out.println(incrList(L, 3));
-        // System.out.println(dincrList(L, 3));        
+        System.out.println(incrList(L, 3));
+        System.out.println(dincrList(L, 3));        
     }
 }
